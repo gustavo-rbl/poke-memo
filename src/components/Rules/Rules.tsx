03104728e0d42dd5142558c18c46dd13236985a7
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-17 17:57:26 -0500

    add Rules

---
+++ src/components/Rules/Rules.tsx
@@ -0,0 +1,17 @@
+function Rules() {
+  return (
+    <>
+      <h4>How to play:</h4>
+
+      <ul>
+        <li>The cards are shuffled and placed on the playing area.</li>
+
+        <li>Picking a card that has already been picked beforehand results in a loss.</li>
+
+        <li>The game continues until all the cards have been matched.</li>
+      </ul>
+    </>
+  );
+}
+
+export default Rules;
