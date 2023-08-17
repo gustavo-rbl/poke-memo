ef7c081b42ab527ad3f8ad2947c3d7ddfe53ae7b
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-17 17:57:35 -0500

    add About

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import Rules from "../../components/Rules/Rules";
+
+function About() {
+  return (
+    <>
+      <h2>About</h2>
+
+      <p>
+        PokeMemo is a classic memory or concentration game. The game consist of a set of cards
+        featuring up to 251 Pokemon{" "}
+        <Link
+          to="https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number"
+          target="_blank"
+        >
+          (up to gen 2)
+        </Link>
+        .
+      </p>
+
+      <Rules />
+
+      <h4>Acknowledgment:</h4>
+      <p>
+        PokeMemo was made possible thanks to{" "}
+        <Link to="https://pokeapi.co/" target="_blank">
+          Pokemon API
+        </Link>
+      </p>
+    </>
+  );
+}
+
+export default About;
