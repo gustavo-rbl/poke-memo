826b789c674f8da979aa7c06e9fddd7b5ecfd630
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-18 18:57:47 -0500

    add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+function Loader() {
+  return (
+    <div className="loading inline--block">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loader;
